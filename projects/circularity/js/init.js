2cec9b2099adf3fe16e2c7c1ec367d84f7051b4e
Ubuntu <ubuntu@ip-172-31-47-3.ec2.internal>
2020-07-18 16:14:44 +0000

    create awesome platformer game

---
+++ projects/circularity/js/init.js
@@ -0,0 +1,172 @@
+var init = function (window) {
+    'use strict';
+    var 
+        draw = window.opspark.draw,
+        physikz = window.opspark.racket.physikz,
+        
+        app = window.opspark.makeApp(),
+        canvas = app.canvas, 
+        view = app.view,
+        fps = draw.fps('#000');
+        
+    
+    window.opspark.makeGame = function() {
+        
+        window.opspark.game = {};
+        var game = window.opspark.game;
+        var circles;
+        
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM SETUP ////////////////////////////
+        ////////////////////////////////////////////////////////////
+        function randomNumberBetween(min, max) {
+    var difference = max - min;
+    var randomValue = Math.random() * difference;
+    return min + randomValue;
+}
+
+    var circles = []; 
+        
+ var circle1 = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+view.addChild(circle1);
+
+
+
+circle1.velocityX = randomNumberBetween(-1, 1);
+circle1.velocityY = randomNumberBetween(-1, 1);
+
+circles.push(circle1);
+
+var circle2 = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+view.addChild(circle2);
+
+circle2.velocityX = randomNumberBetween(-1, 1);
+circle2.velocityY = randomNumberBetween(-1, 1);
+
+circles.push(circle2);
+
+var circle3 = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+view.addChild(circle3);
+
+circle3.velocityX = randomNumberBetween(-1, 1);
+circle3.velocityY = randomNumberBetween(-1, 1);
+
+circles.push(circle3);
+
+var circle4 = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+view.addChild(circle4);
+
+circle4.velocityX = randomNumberBetween(-1, 1);
+circle4.velocityY = randomNumberBetween(-1, 1);
+
+circles.push(circle4);
+
+var circle5 = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+view.addChild(circle5);
+
+circle5.velocityX = randomNumberBetween(-1, 1);
+circle5.velocityY = randomNumberBetween(-1, 1);
+
+circles.push(circle5);
+
+for (var count = 1; count <= 100; count++) {
+    // code block for the for loop
+    var circleX = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+    view.addChild(circleX);
+    circleX.velocityX = randomNumberBetween(-1, 1);
+    circleX.velocityY = randomNumberBetween(-1, 1);
+    circles.push(circleX);
+}
+
+
+    
+
+
+
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM LOGIC ////////////////////////////
+        ////////////////////////////////////////////////////////////
+        
+        /* 
+        This Function is called 60 times/second producing 60 frames/second.
+        In each frame, for every circle, it should redraw that circle
+        and check to see if it has drifted off the screen.         
+        */
+        function update() {
+            
+ circle1.x = circle1.x + circle1.velocityX;
+circle1.y = circle1.y + circle1.velocityY;
+
+circle2.x = circle2.x + circle2.velocityX;
+circle2.y = circle2.y + circle2.velocityY;
+
+circle3.x = circle3.x + circle3.velocityX;
+circle3.y = circle3.y + circle3.velocityY;
+
+circle4.x = circle4.x + circle4.velocityX;
+circle4.y = circle4.y + circle4.velocityY;
+
+circle5.x = circle5.x + circle5.velocityX;
+circle5.y = circle5.y + circle5.velocityY;
+
+game.checkCirclePosition(circle1);
+game.checkCirclePosition(circle2);
+game.checkCirclePosition(circle3);
+game.checkCirclePosition(circle4);
+game.checkCirclePosition(circle5);
+
+for (var i = 0; i <= circles.length-1; i++) { 
+    circles[i].x = circles[i].x + circles[i].velocityX;
+    circles[i].y = circles[i].y + circles[i].velocityY;
+    game.checkCirclePosition(circles[i]);
+}
+            
+        }
+    
+        /* 
+        This Function should check the position of a circle that is passed to the 
+        Function. If that circle drifts off the screen, this Function should move
+        it to the opposite side of the screen.
+        */
+        game.checkCirclePosition = function(circle) {
+
+            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+            
+if (circle.x - circle.radius > canvas.width ) {
+        circle.x = 0 - circle.radius;
+     }
+if (circle.x + circle.radius < 0) {
+         circle.x = canvas.width + circle.radius;
+    }
+if (circle.y - circle.radius > canvas.height) {
+         circle.y = 0 - circle.radius;
+    }
+if (circle.y + circle.radius < 0) {
+         circle.y = canvas.height + circle.radius;
+    }
+            
+            
+        }
+        
+        /////////////////////////////////////////////////////////////
+        // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
+        /////////////////////////////////////////////////////////////
+        
+        view.addChild(fps);
+        app.addUpdateable(fps);
+        
+        
+        game.circles = circles;
+    
+        game.update = update;
+        
+        app.addUpdateable(window.opspark.game);
+    }
+};
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = init;
+}
